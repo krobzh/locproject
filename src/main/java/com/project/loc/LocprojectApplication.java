fb316d16221e8f53989285ff7fa7e601833f4b39
Thomas Croguennec <thomas.croguennec2013@campus-eni.fr>
2016-12-21 15:57:57 +0100

    init project

---
+++ src/main/java/com/project/loc/LocprojectApplication.java
@@ -0,0 +1,12 @@
+package com.project.loc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocprojectApplication.class, args);
+	}
+}
